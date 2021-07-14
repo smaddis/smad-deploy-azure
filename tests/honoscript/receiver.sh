2e8dd53ab87fbe55ed697e9c1bc7748143f661b6
Mikael Saarinen <mikael.saarinen@oulu.fi>
2021-07-14 16:08:14 +0300

    Kafka support for Hono tests
    * Support for Hono Helm chart built-in kafka cluster
    * Update hono-cli to v1.9.0
    Signed-off-by: Mikael Saarinen <mikael.saarinen@oulu.fi>

--- tests/honoscript/receiver.sh
+++ tests/honoscript/receiver.sh
@@ -4,13 +4,19 @@ info=./config.json
 
 test -f $info || exit 1
 
-AMQP_NETWORK_IP=$(jq -r .AMQP_NETWORK_IP < $info)
+KAFKA_IP=$(jq -r .KAFKA_IP < $info)
 MY_TENANT=$(jq -r .MY_TENANT < $info)
+KAFKA_TRUSTSTORE_PATH=$(jq -r .KAFKA_TRUSTSTORE_PATH < $info)
 
-java -jar hono-cli-1.6.0-exec.jar \
---hono.client.host=$AMQP_NETWORK_IP \
---hono.client.port=15672 \
---hono.client.username=consumer@HONO \
---hono.client.password=verysecret \
---spring.profiles.active=receiver \
---tenant.id=$MY_TENANT
+java -jar hono-cli-1.9.0-exec.jar \
+--spring.profiles.active=receiver,local,kafka \
+--tenant.id=$MY_TENANT \
+--hono.kafka.commonClientConfig.bootstrap.servers=$KAFKA_IP:9094 \
+--hono.kafka.commonClientConfig.security.protocol=SASL_SSL \
+--hono.kafka.commonClientConfig.sasl.jaas.config="org.apache.kafka.common.security.scram.ScramLoginModule required username=\"hono\" password=\"hono-secret\";" \
+--hono.kafka.commonClientConfig.sasl.mechanism=SCRAM-SHA-512 \
+--hono.kafka.commonClientConfig.ssl.truststore.location=$KAFKA_TRUSTSTORE_PATH \ 
+--hono.kafka.commonClientConfig.ssl.truststore.password=honotrust \
+--hono.kafka.commonClientConfig.ssl.endpoint.identification.algorithm="" 
+#--hono.client.username=consumer@HONO \
+#--hono.client.password=verysecret \
