fc958a7ae2c2360a036649220cbdff78fff8a53c
Nindemic <niina.lunden@oulu.fi>
2021-07-27 17:34:27 +0300

    Add ambassador mappings for kafka cluster
    Signed-off-by: Nindemic <niina.lunden@oulu.fi>

--- tests/honoscript/receiver.sh
+++ tests/honoscript/receiver.sh
@@ -5,18 +5,19 @@ info=./config.json
 test -f $info || exit 1
 
 KAFKA_IP=$(jq -r .KAFKA_IP < $info)
+#KAFKA_IP=$"workspace-k8stest.westeurope.cloudapp.azure.com"
 MY_TENANT=$(jq -r .MY_TENANT < $info)
 KAFKA_TRUSTSTORE_PATH=$(jq -r .KAFKA_TRUSTSTORE_PATH < $info)
 
 java -jar hono-cli-1.9.0-exec.jar \
 --spring.profiles.active=receiver,local,kafka \
 --tenant.id=$MY_TENANT \
---hono.kafka.commonClientConfig.bootstrap.servers=$KAFKA_IP:9094 \
+--hono.kafka.commonClientConfig.bootstrap.servers=$KAFKA_IP:9092 \
 --hono.kafka.commonClientConfig.security.protocol=SASL_SSL \
 --hono.kafka.commonClientConfig.sasl.jaas.config="org.apache.kafka.common.security.scram.ScramLoginModule required username=\"hono\" password=\"hono-secret\";" \
 --hono.kafka.commonClientConfig.sasl.mechanism=SCRAM-SHA-512 \
---hono.kafka.commonClientConfig.ssl.truststore.location=$KAFKA_TRUSTSTORE_PATH \ 
+--hono.kafka.commonClientConfig.ssl.truststore.location=$KAFKA_TRUSTSTORE_PATH \
 --hono.kafka.commonClientConfig.ssl.truststore.password=honotrust \
---hono.kafka.commonClientConfig.ssl.endpoint.identification.algorithm="" 
+--hono.kafka.commonClientConfig.ssl.endpoint.identification.algorithm=""
 #--hono.client.username=consumer@HONO \
 #--hono.client.password=verysecret \
