5b1d81b672bd812726f149ab12f921f6c1057af7
Mikael Saarinen <mikael.saarinen@oulu.fi>
2021-12-29 16:26:48 +0200

    Fix hono auth with custom kafka
    Signed-off-by: Mikael Saarinen <mikael.saarinen@oulu.fi>

--- tests/honoscript/receiver.sh
+++ tests/honoscript/receiver.sh
@@ -13,11 +13,11 @@ java -jar hono-cli-1.9.0-exec.jar \
 --spring.profiles.active=receiver,local,kafka \
 --tenant.id=$MY_TENANT \
 --hono.kafka.commonClientConfig.bootstrap.servers=${DOMAIN_NAME}:${KAFKA_PORT} \
---hono.kafka.commonClientConfig.security.protocol=SASL_SSL \
+--hono.kafka.commonClientConfig.security.protocol=SASL_PLAINTEXT \
 --hono.kafka.commonClientConfig.sasl.jaas.config="org.apache.kafka.common.security.scram.ScramLoginModule required username=\"hono\" password=\"hono-secret\";" \
---hono.kafka.commonClientConfig.sasl.mechanism=SCRAM-SHA-512 \
---hono.kafka.commonClientConfig.ssl.truststore.location=$KAFKA_TRUSTSTORE_PATH \
---hono.kafka.commonClientConfig.ssl.truststore.password=honotrust \
---hono.kafka.commonClientConfig.ssl.endpoint.identification.algorithm=""
+--hono.kafka.commonClientConfig.sasl.mechanism=SCRAM-SHA-512 
+#--hono.kafka.commonClientConfig.ssl.truststore.location=$KAFKA_TRUSTSTORE_PATH \
+#--hono.kafka.commonClientConfig.ssl.truststore.password=honotrust \
+#--hono.kafka.commonClientConfig.ssl.endpoint.identification.algorithm=""
 #--hono.client.username=consumer@HONO \
 #--hono.client.password=verysecret \
