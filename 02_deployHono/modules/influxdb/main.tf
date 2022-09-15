3476e70eca2c54646528996ef4bcce4fbc5f1010
Niina Lunden <niina.lunden@oulu.fi>
2022-09-15 14:58:32 +0300

    Change influxdb helm chart provider
    Signed-off-by: Niina Lunden <niina.lunden@oulu.fi>

--- 02_deployHono/modules/influxdb/main.tf
+++ 02_deployHono/modules/influxdb/main.tf
@@ -1,17 +1,12 @@
 # https://github.com/bitnami/charts/tree/3f2b19ad2743d28828b5c217b2b682e0db57be75/bitnami/influxdb
 resource "helm_release" "influx" {
   name       = "influx"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://helm.influxdata.com/"
   chart      = "influxdb"
-  version    = "1.1.8"
+  version    = "4.12.0"
 
   values = [
     file("${path.module}/values.yaml")
   ]
-
-  set {
-    name  = "image.tag"
-    value = "1.8.4"
-  }
 
 }
